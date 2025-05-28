1569f7d7b91a961f957284baf36b3d2080d004e8
Mudit Jain <muditjain.1406@gmail.com>
2025-05-28 15:49:12 +0530

    final

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
-const email = document.getElementById("email")
-const err = document.querySelector(".error-frame span")
-const form = document.getElementById("form")
-const signUpPage = document.getElementById("signUpPage")
-const successPage = document.querySelector(".success")
+const formCard = document.getElementById('formCard');
+const form = document.getElementById('subscribeForm');
+const errMsg = document.getElementById('emailError');
+const emailInput = document.getElementById('email');
+const emailOutput = document.getElementById('userEmail');
+const dismiss = document.getElementById('dismiss');
+const successCard = document.getElementById('successCard');
+function validateEmail(email){
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(email);
+}
 
-email.addEventListener("input", (event) => {
-  if (email.validity.valid) {
-    err.className = "error hidden";
-  } else {
-    err.className = "error";
-  }
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = emailInput.value.trim();
+    if(!validateEmail(email)){
+        errMsg.classList.remove("hidden");
+    }
+    else {
+        errMsg.classList.add("hidden");
+        formCard.style.display = 'none';
+        emailOutput.textContent = email;
+        successCard.style.display = 'flex';
+    }
 });
 
-form.addEventListener("submit", event=>{
-    if(!email.validity.valid){
-        err.className = "error"
-        event.preventDefault()
-    }
-    else{
-        event.preventDefault()
-        signUpPage.classList.add("hidden")
-        successPage.classList.remove("hidden")
-
-    }
-})
+dismiss.addEventListener('click', () => {
+    successCard.style.display = 'none';
+    formCard.style.display = 'flex';
+    emailInput.value = '';
+});
